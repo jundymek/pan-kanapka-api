27e3ea96a7201ba3df9f9ae2eec46e331fe5cdc7
jundymek <jundymek@gmail.com>
2019-11-17 22:31:11 +0100

    feat: add Place model

--- kanapka/models.py
+++ kanapka/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Place(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
--- pan_kanapka/settings.py
+++ pan_kanapka/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kanapka'
+    'kanapka',
+    'django_extensions'
 ]
 
 MIDDLEWARE = [
