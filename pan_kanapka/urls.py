1133b349aa6fdd96d3f1a1450ed2306c0be21593
jundymek <jundymek@gmail.com>
2020-02-12 09:13:57 +0100

    feat: change media urlpatterns

--- pan_kanapka/urls.py
+++ pan_kanapka/urls.py
@@ -43,5 +43,4 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
